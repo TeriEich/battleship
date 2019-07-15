41f412f8143a5f8fb28a2af00264f9703b74a1f8
Teri <teri.eich@gmail.com>
2019-07-15 20:28:09 +0000

    worked on the basic HUD display

--- src/HUD.jsx
+++ src/HUD.jsx
@@ -4,10 +4,22 @@ function HUD(props) {
 
   return (
     <footer className="hud">
-      <input className="hud-controls"
-        type="text"
-        name="attackCoordinates"
-        placeholder={"coordinates"} />
+      <div className="hud-ship-list">
+        <h2>Your Ships:</h2>
+      </div>
+      <div className="hud-controls">
+        <h2>Attack!</h2>
+        <input
+          type="text"
+          name="attackCoordinates"
+          placeholder={"coordinates"}
+        />
+      </div>
+      <div className="hud-turn-info">
+        <h2>Turn Info:</h2>
+        <h3>Turn Number [5]</h3>
+        <h3>[Player 2] is deciding where to attack.</h3>
+      </div>
     </footer>
   );
 }
